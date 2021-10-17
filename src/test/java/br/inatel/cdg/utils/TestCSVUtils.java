3fdf3b41afaf56645ba81de9f7016fa20d1dafb1
franlaranjo <88806125+franlaranjo@users.noreply.github.com>
2021-10-17 16:24:12 -0300

    Revert "Teste de Unidade da classe jogo controller"
    This reverts commit 7c6f5335596a4392a5eb67495a627b142ada8652.

--- src/test/java/br/inatel/cdg/utils/TestCSVUtils.java
+++ src/test/java/br/inatel/cdg/utils/TestCSVUtils.java
@@ -1,13 +1,10 @@
 package br.inatel.cdg.utils;
 
 import br.inatel.cdg.model.Jogo;
-import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,22 +21,8 @@ public class TestCSVUtils {
     }
 
     @Test
-    public void testNumberOfLines() {
+    public void testNumberOfLines(){
         int numLines = jogoList.size();
-        Assert.assertEquals(100, numLines);
-    }
-
-    @Test
-    public void testEscreverArquivos() throws URISyntaxException, InterruptedException {
-        CsvUtils.escreverArquivo(jogoList);
-
-        //esperando 2 segundos para o arquivo ser criado
-        Thread.sleep(2000);
-
-        Path caminho = Paths.get(ClassLoader.getSystemResource("vendas-games-filtrados.csv").toURI());
-        List<Jogo> novoArquivojogoList = CsvUtils.lerArquivoCsv(caminho);
-
-        int numLines = novoArquivojogoList.size();
-        Assert.assertEquals(100, numLines);
+        Assert.assertEquals(100,numLines);
     }
 }
